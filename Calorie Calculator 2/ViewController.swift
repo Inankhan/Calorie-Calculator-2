d5dff7298bd6a21e9e2b37192ae6bddb70041efd
Inan <mik37@scarletmail.rutgers.edu>
2021-05-09 17:34:46 -0400

    Initial Commit

---
+++ Calorie Calculator 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calorie Calculator 2
+//
+//  Created by Inan Khan on 5/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
